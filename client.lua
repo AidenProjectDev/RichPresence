4372348a1d64fe839c8c6aa57b7f56de8c6ae0b0
CapitaineFlam06 <44647258+CapitaineFlam06@users.noreply.github.com>
2019-08-09 20:40:42 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -15,7 +15,7 @@ Citizen.CreateThread(function()
         SetDiscordRichPresenceAssetSmall('NOM DE LA PETITE IMAGE')
 
         --Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('NOM AFFICHER DE LA PETITE IMAGE')
+        SetDiscordRichPresenceAssetSmallText('NOM AFFICHÉ DE LA PETITE IMAGE')
 
   players = {}
   for i = 0, 32 do -- Mettre 0, 255 pour du 64 slots
@@ -23,7 +23,7 @@ Citizen.CreateThread(function()
           table.insert( players, i )
       end
   end
-  SetRichPresence(GetPlayerName(PlayerId()) .. " - ".. #players .. "/32")
+  SetRichPresence(GetPlayerName(PlayerId()) .. " - ".. #players .. "/32") -- Remplacer 32 par le nombre de joueurs
   
         --It updates every one minute just in case.
 		Citizen.Wait(60000)
